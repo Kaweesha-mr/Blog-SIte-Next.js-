05ab16f5e3bc060e6710f5fc077188c006324c50
= <kaweesha.mr@gmail.com>
2023-12-16 16:41:49 +0530

    Fix bug in login functionality

--- dojo-helpdesk/app/components/NavBar.jsx
+++ dojo-helpdesk/app/components/NavBar.jsx
@@ -1,9 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
+import logo from '../components/dojo-logo.png'
 
 export default function NavBar() {
   return (
     <nav> 
+      <Image 
+        src = {logo}
+        alt = 'Dojo helpdesk logo'
+        width = {70}
+        quality = {100}
+        placeholder = 'blur'
+        />
+
     <h1>Help Desk</h1>
     <Link href="/">Dashbord</Link>
     <Link href="/tickets">Tickets</Link>
