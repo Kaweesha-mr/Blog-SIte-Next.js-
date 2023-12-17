b84dd6f012fc2f9026b6e9dc83448d4ddc8a6429
= <kaweesha.mr@gmail.com>
2023-12-17 18:27:34 +0530

    finidhed next 13 course

--- dojo-helpdesk/app/tickets/create/page.jsx
+++ dojo-helpdesk/app/tickets/create/page.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import CreateForm from "./CreateForm";
 
-export default function CreateForm() {
+export default function CreateTicket() {
   return (
 
-    
+   <main>
+    <h2 className="text-primary text-center">Add New Ticket</h2>
+    <CreateForm/>
+   </main>
+
+
   )
 }
